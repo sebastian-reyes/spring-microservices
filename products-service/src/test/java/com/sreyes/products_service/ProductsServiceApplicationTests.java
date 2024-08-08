7649eaa313764a74498345def2771a1acb4ab75f
sebastian-reyes <sebastianreyes2105@gmail.com>
2024-08-07 22:08:26 -0500

    Agregando microservicios para inventario, orden y productos

---
+++ products-service/src/test/java/com/sreyes/products_service/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreyes.products_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
